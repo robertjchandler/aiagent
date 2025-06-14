a3f94e1514000b7fb1569b378a74bea1199f4294
robertjchandler <robertjchandler4@gmail.com>
2025-06-14 17:42:24 -0500

    update main

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv("apikey.env")
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    api_key = os.environ.get("GEMINI_API_KEY")    
+    client = genai.Client(api_key=api_key)    
+
+    if len(sys.argv) < 2:
+        print("No prompt was provided.")
+        sys.exit(1)
+    
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     print("Response:")
     print(response.text)
 
