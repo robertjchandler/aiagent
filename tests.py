9315f37dcd0b2f3c712114452c9995e65480e3fe
robertjchandler <robertjchandler4@gmail.com>
2025-06-19 17:27:26 -0500

    update tests

--- tests.py
+++ tests.py
@@ -1,22 +1,8 @@
 # tests.py
 
-# from functions.write_file import write_file
 from functions.run_python import run_python_file
 
 def test():
-    """
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result of 'lorem.txt'")
-    print(result)
-
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result of 'pkg/morelorem.txt'")
-    print(result)
-
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result of '/tmp/temp.txt'")
-    print(result)
-    """
     result = run_python_file("calculator", "main.py")
     print("Result of 'main.py'")
     print(result)
