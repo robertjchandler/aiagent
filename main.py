53c430acb098dce0acb3d53dd1ed7b1b3272acaa
robertjchandler <robertjchandler4@gmail.com>
2025-06-24 17:00:05 -0500

    add feedback loop

--- main.py
+++ main.py
@@ -10,39 +10,48 @@ from functions.call_function import call_function
 def main():
     load_dotenv("apikey.env")
     api_key = os.environ.get("GEMINI_API_KEY")    
-    client = genai.Client(api_key=api_key)    
+    client = genai.Client(api_key=api_key)
+    max_iterations = 20
 
     if len(sys.argv) < 2:
         print("No prompt was provided.")
         sys.exit(1)
     
     user_prompt = sys.argv[1]
+    i = 0
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-    )
+    while i < max_iterations:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+        )
 
-    verbose = False
-    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-        verbose = True
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+            
+        verbose = False
+        if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+            verbose = True
 
-    if verbose:
-        print("User prompt:", user_prompt)
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        if verbose:
+            print("User prompt:", user_prompt)
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-            result = call_function(function_call_part, verbose)
-            print(result)
-    else:
-        print("Response:")
-        print(response.text)
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+                result = call_function(function_call_part, verbose)
+                messages.append(result)
+        else:
+            print("Response:")
+            print(response.text)
+            break
+
+        i += 1
 
 if __name__ == "__main__":
     main()
